541bacff9b859ece37146bcbd42ba7469a65aaaf
scubase7en <orchimestates@gmail.com>
2026-04-15 12:08:35 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const STORE_HASH = 'prsxnxsly0';
+const ACCESS_TOKEN = 'nzv1vxafw5v1xu3bvwfzhlzw8zt4ero';
+const BC_BASE = `https://api.bigcommerce.com/stores/${STORE_HASH}/v3`;
+const HEADERS = {
+  'X-Auth-Token': ACCESS_TOKEN,
+  'Content-Type': 'application/json',
+  'Accept': 'application/json'
+};
+
+app.get('/products', async (req, res) => {
+  const keyword = req.query.keyword || '';
+  const url = `${BC_BASE}/catalog/products?keyword=${encodeURIComponent(keyword)}&limit=8&include_fields=id,name,sku,description,page_title,meta_description,search_keywords`;
+  const r = await fetch(url, { headers: HEADERS });
+  const data = await r.json();
+  res.json(data);
+});
+
+app.put('/products/:id', async (req, res) => {
+  const url = `${BC_BASE}/catalog/products/${req.params.id}`;
+  const r = await fetch(url, { method: 'PUT', headers: HEADERS, body: JSON.stringify(req.body) });
+  const data = await r.json();
+  res.json(data);
+});
+
+app.listen(process.env.PORT || 3000);
